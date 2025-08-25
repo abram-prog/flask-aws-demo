dd775f7236d5e67640dfd77392dfec72a9d146e2
Andrew Abramov <andrewa.work.il@gmail.com>
2025-08-25 13:11:39 +0000

    Trigger CI/CD with new message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask inside Docker on AWS!"
+    return "Hello from CI/CD on AWS!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
